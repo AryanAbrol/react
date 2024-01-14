f279edde9a6ffffd72944dda2cb23b15100dc868
Aryan Abrol <aryanabrol95@gmail.com>
2024-01-14 22:08:00 +0530

    kk

--- 02-createreact-comp/create-react-comp/src/App.jsx
+++ 02-createreact-comp/create-react-comp/src/App.jsx
@@ -1,12 +1,12 @@
 import kgbutton from "./kgbutton";
+
 function App()
 {
   return <div>
   <h1>
     this is a react course hain na pka
   </h1>
-
-    <kgbutton></kgbutton>
+  <kgbutton></kgbutton>
   </div> 
 }
 export default App;
